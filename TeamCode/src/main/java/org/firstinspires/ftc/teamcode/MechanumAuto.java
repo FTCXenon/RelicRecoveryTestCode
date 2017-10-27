ff48bb2c2cc3ace28ee8c6168fd432ec6f1d329a
Bridget <bridgetj18@mhs-la.org>
2017-10-27 16:35:34 -0700

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumAuto.java
@@ -0,0 +1,135 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+/**
+ * Created by bridgetmacmillan on 9/11/17.
+ */
+
+@Autonomous(name = "Mechanum Auto")
+public class MechanumAuto extends LinearOpMode{
+
+    DcMotor LF;
+    DcMotor RF;
+    DcMotor LB;
+    DcMotor RB;
+
+    OpenGLMatrix lastLocation = null;
+    VuforiaLocalizer vuforia;
+
+    VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+    VuforiaTrackable relicTemplate = relicTrackables.get(0);
+
+    public void HardwareMap(){
+        LF = hardwareMap.dcMotor.get("LF");
+        RF = hardwareMap.dcMotor.get("RF");
+        LB = hardwareMap.dcMotor.get("LB");
+        RB = hardwareMap.dcMotor.get("RB");
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = "AYh6YDf/////AAAAGZDBru4WXE+AmK7KehDG6RJtFNArHiwXcwwFDhnuZ5mVngFbavJmOWea89Pk12G8YxqKKWn2BI1LB997tYYpps9gzKdsskXu/nB6TJ/yqGT26F3mMKwRC81OnHum3VYIMm7eBUojOVJKrLkumfL4JBr1uxzL56S6r57DaW1LBS1cn4VwPcv0/2ALgoPGmH3LQlcANJjPMqp217o0mN5lApzOsoVbVVlAsVbF9YHcAO9pC+JKOuybJ5NninkoDwBkqhuVcFlmUZnE0YWwherirv7HXG3Y0be0sbQQvVDOcIhpcU4Dz9AujHnMCo9Mjmcq50nOv8yBITbSkJwC3fQZslfZ2jdn/zM3m2Z3RgkGxMaT\n";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+
+        telemetry.addData(">", "Press Play to start");
+        telemetry.update();
+        waitForStart();
+
+        relicTrackables.activate();
+
+    }
+    public void driveForawrd(long driveForward)throws InterruptedException{
+        LF.setPower(1);
+        RF.setPower(1);
+        LB.setPower(1);
+        RB.setPower(1);
+        sleep(driveForward);
+        LF.setPower(0);
+        RF.setPower(0);
+        LB.setPower(0);
+        RB.setPower(0);
+    }
+    public void driveBackward(long DriveBackward)throws InterruptedException{
+        LF.setPower(-1);
+        RF.setPower(-1);
+        LB.setPower(-1);
+        RB.setPower(-1);
+        sleep(DriveBackward);
+        LF.setPower(0);
+        RF.setPower(0);
+        LB.setPower(0);
+        RB.setPower(0);
+    }
+    public void jewelArm(){
+
+    }
+    public void pictographDecoder(){
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
+
+                /* Found an instance of the template. In the actual game, you will probably
+                 * loop until this condition occurs, then move on to act accordingly depending
+                 * on which VuMark was visible. */
+            telemetry.addData("VuMark", "%s visible", vuMark);
+
+                /* For fun, we also exhibit the navigational pose. In the Relic Recovery game,
+                 * it is perhaps unlikely that you will actually need to act on this pose information, but
+                 * we illustrate it nevertheless, for completeness. */
+            OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
+            telemetry.addData("Pose", format(pose));
+
+                /* We further illustrate how to decompose the pose into useful rotational and
+                 * translational components */
+            if (pose != null) {
+                VectorF trans = pose.getTranslation();
+                Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+                // Extract the X, Y, and Z components of the offset of the target relative to the robot
+                double tX = trans.get(0);
+                double tY = trans.get(1);
+                double tZ = trans.get(2);
+
+                // Extract the rotational components of the target relative to the robot
+                double rX = rot.firstAngle;
+                double rY = rot.secondAngle;
+                double rZ = rot.thirdAngle;
+            }
+        }
+        else {
+            telemetry.addData("VuMark", "not visible");
+        }
+        telemetry.update();
+    }
+
+    String format(OpenGLMatrix transformationMatrix) {
+        return (transformationMatrix != null) ? transformationMatrix.formatAsTransform() : "null";
+    }
+
+    @Override public void runOpMode(){
+        HardwareMap();
+        waitForStart();
+        while (opModeIsActive()) {
+            //CODE GOES HERE
+
+
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumTeleop.java
@@ -0,0 +1,121 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcontroller.external.samples.ConceptVuforiaNavigation;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+
+/**
+ * Created by bridgetmacmillan on 9/26/17.
+ */
+
+@TeleOp(name = "Mechanum Teleop")
+public class MechanumTeleop extends LinearOpMode{
+
+    DcMotor LF;
+    DcMotor RF;
+    DcMotor LB;
+    DcMotor RB;
+
+    double speed;
+    double pi = Math.PI;
+    double angle;
+    double dchange;
+    double posRF, posLF, posRB, posLB, maxValue;
+    double leftX;
+    double leftY;
+    double motorPower;
+
+    public static final String TAG = "Vuforia VuMark Sample";
+    OpenGLMatrix lastLocation = null;
+    VuforiaLocalizer vuforia;
+
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        LF = hardwareMap.dcMotor.get("LF");
+        RF = hardwareMap.dcMotor.get("RF");
+        LB = hardwareMap.dcMotor.get("LB");
+        RB = hardwareMap.dcMotor.get("RB");
+
+        LB.setDirection(DcMotor.Direction.REVERSE);
+        RB.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+
+        while (opModeIsActive()) {
+
+            //MECHANUM DRIVE
+            leftX = gamepad1.left_stick_x;
+            leftY = gamepad1.left_stick_y;
+            angle = Math.atan2(leftX, -leftY) - (pi / 2);
+            speed = Math.sqrt(leftX * leftX + leftY * leftY);
+            dchange = -(leftX * leftX) / 3.33;
+            posRF = speed * Math.sin(angle + (Math.PI / 4)) + dchange;
+            posLF = speed * Math.cos(angle + (Math.PI / 4)) + dchange;
+            posRB = speed * Math.cos(angle + (Math.PI / 4)) - dchange;
+            posLB = speed * Math.sin(angle + (Math.PI / 4)) - dchange;
+            //Set maxValue to pos1 absolute
+            maxValue = Math.abs(posRF);
+
+            //If pos2 absolute is greater than maxValue, then make maxValue equal to pos2 absolute
+            if (Math.abs(posLF) > maxValue) {
+                maxValue = Math.abs(posLF);
+            }
+            //If pos2 absolute is greater than maxValue, then make maxValue equal to pos2 absolute
+            if (Math.abs(posLF) > maxValue) {
+                maxValue = Math.abs(posLF);
+            }
+            //If pos3 absolute is greater than maxValue, then make maxValue equal to pos3 absolute
+            if (Math.abs(posRB) > maxValue) {
+                maxValue = Math.abs(posRB);
+            }
+            //If pos4 absolute is greater than maxValue, then make maxValue equal to pos4 absolute
+            if (Math.abs(posLB) > maxValue) {
+                maxValue = Math.abs(posLB);
+            }
+
+            //Check if need to scale -- if not set to 1 to nullify scale
+            if (maxValue <= 1) {
+                maxValue = 1;
+            }
+            RF.setPower(posRF / maxValue);
+            LF.setPower(posLF / maxValue);
+            RB.setPower(posRB / maxValue);
+            LB.setPower(posLB / maxValue);
+            //Rotate
+            //Clockwise
+            while (gamepad1.right_stick_x > 0) {
+                motorPower = Math.abs(gamepad1.right_stick_x);
+                LF.setPower(motorPower);
+                RF.setPower(motorPower);
+                LB.setPower(-motorPower);
+                RB.setPower(-motorPower);
+            }
+            //Counter-Clockwise
+            while (gamepad1.right_stick_x < 0) {
+                motorPower = Math.abs(gamepad1.right_stick_x);
+                LF.setPower(-motorPower);
+                LB.setPower(motorPower);
+                RF.setPower(-motorPower);
+                RB.setPower(motorPower);
+            }
+
+
+        }
+    }
+}
